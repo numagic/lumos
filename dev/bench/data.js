a24057e4125217788bd694b4658e06c9ab593766
github-action-benchmark <github@users.noreply.github.com>
2022-03-31 22:13:45 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 2dfbb47b9cae0f815f1c8bc0df39a92b794d68cb

---
+++ dev/bench/data.js
@@ -0,0 +1,43 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1648764825862,
+  "repoUrl": "https://github.com/numagic/lumos",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "numagic",
+            "username": "numagic"
+          },
+          "committer": {
+            "name": "numagic",
+            "username": "numagic"
+          },
+          "id": "2dfbb47b9cae0f815f1c8bc0df39a92b794d68cb",
+          "message": "add laptime simulation regression test",
+          "timestamp": "2022-03-15T14:25:23Z",
+          "url": "https://github.com/numagic/lumos/pull/4/commits/2dfbb47b9cae0f815f1c8bc0df39a92b794d68cb"
+        },
+        "date": 1648764824765,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "num_total",
+            "value": 25,
+            "unit": "-"
+          },
+          {
+            "name": "failure_pct",
+            "value": 0,
+            "unit": "%"
+          },
+          {
+            "name": "avg_success_iter",
+            "value": 56.2,
+            "unit": "iter"
+          }
+        ]
+      }
+    ]
+  }
+}
