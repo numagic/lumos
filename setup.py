5f4d772d52e362f22dc08bd0654a3f7839bc8a3e
yunlongxu <yunlong@numagic.io>
2022-04-20 21:10:26 +0000

    pre-release 0.0.2rc1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="numagic-lumos",
-    version="0.0.2c",
+    version="0.0.2rc1",
     author="Yunlong Xu",
     author_email="yunlong@numagic.io",
     description="lumos - scalable accelerated optimal control",
