584289bd91641838225db567e16c6583bc581d13
Yunlong Xu <92360845+yunlongxu-numagic@users.noreply.github.com>
2022-06-01 13:51:24 +0200

    remove dummy metric

--- dev/bench-features/data.js
+++ dev/bench-features/data.js
@@ -16,35 +16,6 @@ window.BENCHMARK_DATA = {
             "username": "yunlongxu-numagic"
           },
           "distinct": true,
-          "id": "a5b8bf33e247ad4fcfa6a99a9d619053981ec614",
-          "message": "use same branch but different folder",
-          "timestamp": "2022-06-01T11:03:29Z",
-          "tree_id": "4e2b70da548d38681029833e42f3b5259e285424",
-          "url": "https://github.com/numagic/lumos/commit/a5b8bf33e247ad4fcfa6a99a9d619053981ec614"
-        },
-        "date": 1654081565668,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "test_metric",
-            "value": 1.23,
-            "unit": "sec"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "yunlong@numagic.io",
-            "name": "yunlongxu",
-            "username": "yunlongxu-numagic"
-          },
-          "committer": {
-            "email": "yunlong@numagic.io",
-            "name": "yunlongxu",
-            "username": "yunlongxu-numagic"
-          },
-          "distinct": true,
           "id": "3ee2f1c03341248e39420ad515b2725f749af998",
           "message": "restore the real tests",
           "timestamp": "2022-06-01T11:07:45Z",
