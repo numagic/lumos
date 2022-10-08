6bfe65058cd79ec77489387b2dc6ef922f4045c7
yunlongxu <yunlong@numagic.io>
2022-10-08 13:52:23 +0000

    bump version to 0.0.2rc7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="numagic-lumos",
-    version="0.0.2rc6",
+    version="0.0.2rc7",
     author="Yunlong Xu",
     author_email="yunlong@numagic.io",
     description="lumos - scalable accelerated optimal control",
